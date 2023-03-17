a889f8784bf0c001f77e09efba83e7220965e430
Housam Hegazy <geohousam@gmail.com>
2023-03-17 20:14:35 +0000

    Update Cart.jsx and Home.jsx

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,8 +1,24 @@
-import { Box, Button, InputAdornment, TextField, styled } from "@mui/material";
+import {
+  Box,
+  Button,
+  InputAdornment,
+  Stack,
+  TextField,
+  styled,
+  Typography,
+  ButtonGroup,
+  IconButton,
+} from "@mui/material";
 import "./Cart.css";
 import React from "react";
 import { purple } from "@mui/material/colors";
 import { ChevronRight } from "@mui/icons-material";
+import Badge from "@mui/material/Badge";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import MailIcon from "@mui/icons-material/Mail";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 const ColorButton = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(purple[500]),
@@ -11,40 +27,66 @@ const ColorButton = styled(Button)(({ theme }) => ({
   "&:hover": {
     // @ts-ignore
     backgroundColor: theme.palette.ali.main,
-    scale: "0.99"
+    scale: "0.99",
   },
 }));
 
-export default function MyCart(){
-  
+export default function MyCart() {
+  const [count, setCount] = React.useState(1);
+  const [invisible, setInvisible] = React.useState(false);
+
+  const handleBadgeVisibility = () => {
+    setInvisible(!invisible);
+  };
   return (
-    <Box sx={{ width: "380px" }} component="form">
-      <TextField
-        fullWidth={true}
-        label="Transaction Title"
-        sx={{ mt: "22px", display: "block" }}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">👉</InputAdornment>,
-        }}
-        variant="filled"
-      />
-
-      <TextField
-        fullWidth={true}
-        label="Transaction Title"
-        id="filled-start-adornment"
-        sx={{ mt: "22px", display: "block" }}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-        }}
-        variant="filled"
-      />
-
-      <ColorButton sx={{ mt: "22px" }} variant="contained">
-        Submit <ChevronRight />
-      </ColorButton>
-    </Box>
+    <Stack>
+      <Box sx={{ border: "2px solid red" }}>
+        <Button variant="text" color="error">
+          Delete
+        </Button>
+        <Typography>$ 100</Typography>
+        <Box
+          sx={{
+            color: "action.active",
+            display: "flex",
+            flexDirection: "column",
+            "& > *": {
+              marginBottom: 2,
+            },
+            "& .MuiBadge-root": {
+              marginRight: 4,
+            },
+          }}
+        >
+          <div>
+            <ButtonGroup>
+              <Button
+                aria-label="reduce"
+                onClick={() => {
+                  setCount(Math.max(count - 1, 0));
+                }}
+              >
+                <RemoveIcon fontSize="small" />
+              </Button>
+              <Box>
+                <Badge
+                  sx={{ position: "relative" }}
+                  color="secondary"
+                  badgeContent={count}
+                ></Badge>
+              </Box>
+              <Button
+                aria-label="increase"
+                onClick={() => {
+                  setCount(count + 1);
+                }}
+              >
+                <AddIcon fontSize="small" />
+              </Button>
+            </ButtonGroup>
+          </div>
+        </Box>
+      </Box>
+    </Stack>
   );
-};
-
-
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/system";
 import "./Home.css";
 import React from "react";
-import { Typography, useTheme, IconButton, Button } from "@mui/material";
+import { Typography, useTheme, IconButton, Stack, Button } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
@@ -14,33 +14,49 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 // import { useTheme } from "@emotion/react";
+const recivedDatafromApi = [{}, {}, {}, {}];
 const Home = () => {
   const theme = useTheme();
   return (
-    <Box>
-      <Card sx={{ maxWidth: 277 ,mx:2,mb:6}}>
-        <CardMedia
-          component="img"
-          height="194"
-          image="/static/images/cards/paella.jpg"
-          alt="Paella dish"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with
-          </Typography>
-        </CardContent>
-        <CardActions sx={{justifyContent:"space-between"}} disableSpacing>
-          <Button sx={{textTransform:'capitalize'}} variant="contained" color="primary">
-            Add to Cart
-          </Button>
-          <Typography sx={{mr:1}} variant="body1" color={theme.palette.error.main}>
-            $ 100
-          </Typography>
-        </CardActions>
-      </Card>
-    </Box>
+    <Stack direction="row" sx={{ flexWrap: "wrap",justifyContent:'center' }}>
+      {recivedDatafromApi.map((item) => {
+        return (
+          <Card sx={{ maxWidth: 277, mx: 2, mb: 6 }}>
+            <CardMedia
+              component="img"
+              height="194"
+              image="/static/images/cards/paella.jpg"
+              alt="Paella dish"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to
+                cook together with
+              </Typography>
+            </CardContent>
+            <CardActions
+              sx={{ justifyContent: "space-between" }}
+              disableSpacing
+            >
+              <Button
+                sx={{ textTransform: "capitalize",lineHeight:1 }}
+                variant="contained"
+                color="primary"
+              >
+                Add to Cart
+              </Button>
+              <Typography
+                sx={{ mr: 1 }}
+                variant="body1"
+                color={theme.palette.error.main}
+              >
+                $ 100
+              </Typography>
+            </CardActions>
+          </Card>
+        );
+      })}
+    </Stack>
   );
 };
 
