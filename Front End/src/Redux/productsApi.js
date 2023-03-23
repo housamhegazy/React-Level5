212b5c6c8b8e9dc8c31ef00db767c6020a626206
housamhegazy <geohousam@gmail.com>
2023-03-23 20:20:11 +0300

    s

--- Front End/src/Redux/productsApi.js
+++ Front End/src/Redux/productsApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: 'productsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://react-level5-plfb.onrender.com/' }),
   endpoints: (builder) => ({
     getproductsByName: builder.query({
       query: (name) => `products`,
